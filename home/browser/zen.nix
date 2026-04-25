72aab5f33660c7dfa9b48b4cc2139ac4ad93eaee
Christoph <christoph@intanet.de>
2026-04-25 14:06:46 +0200

    disable zen welcome screen

--- home/browser/zen.nix
+++ home/browser/zen.nix
@@ -9,22 +9,21 @@
         policies = {
             DisableFirstRun = true;
             DontCheckDefaultBrowser = true;
-            #     AutofillAddressEnabled = true;
-            #     AutofillCreditCardEnabled = false;
-            #     DisableAppUpdate = true;
-            #     DisableFeedbackCommands = true;
-            #     DisableFirefoxStudies = true;
-            #     DisablePocket = true;
+            # AutofillAddressEnabled = true;
+            # AutofillCreditCardEnabled = false;
+            # DisableAppUpdate = true;
+            # DisableFeedbackCommands = true;
+            # DisableFirefoxStudies = true;
+            # DisablePocket = true;
             DisableTelemetry = true;
-            #     DontCheckDefaultBrowser = true;
-            #     NoDefaultBookmarks = true;
-            #     OfferToSaveLogins = false;
-            #     EnableTrackingProtection = {
-            #         Value = true;
-            #         Locked = true;
-            #         Cryptomining = true;
-            #         Fingerprinting = true;
-            #     };
+            # NoDefaultBookmarks = true;
+            # OfferToSaveLogins = false;
+            # EnableTrackingProtection = {
+            #     Value = true;
+            #     Locked = true;
+            #     Cryptomining = true;
+            #     Fingerprinting = true;
+            # };
         };
 
         profiles.default = {
@@ -41,6 +40,7 @@
             ];
 
             settings = {
+                zen.welcome-screen.seen = true;
                 zen.tabs.show-newtab-vertical = false;
                 zen.view.use-single-toolbar = false;
                 zen.view.sidebar-expanded = false;
