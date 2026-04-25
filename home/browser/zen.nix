650d9a93aa6844a8d53662821dfdf31ddf1f3582
Christoph <christoph@intanet.de>
2026-04-25 14:35:38 +0200

    zen browser prefs fixed?

--- home/browser/zen.nix
+++ home/browser/zen.nix
@@ -7,15 +7,16 @@
         enable = true;
 
         policies = {
-            DisableFirstRun = true;
             DontCheckDefaultBrowser = true;
+            OverrideFirstRunPage = "";
+            OverridePostUpdatePage = "";
+            DisableTelemetry = true;
             # AutofillAddressEnabled = true;
             # AutofillCreditCardEnabled = false;
             # DisableAppUpdate = true;
             # DisableFeedbackCommands = true;
             # DisableFirefoxStudies = true;
             # DisablePocket = true;
-            DisableTelemetry = true;
             # NoDefaultBookmarks = true;
             # OfferToSaveLogins = false;
             # EnableTrackingProtection = {
@@ -40,52 +41,18 @@
             ];
 
             settings = {
-                zen.welcome-screen.seen = true;
-                zen.tabs.show-newtab-vertical = false;
-                zen.view.use-single-toolbar = false;
-                zen.view.sidebar-expanded = false;
-                browser.uiCustomization.state = {
-                    placements = {
-                        widget-overflow-fixed-list = [ ];
-                        zen-sidebar-top-buttons = [ "zen-toggle-compact-mode" ];
-                        zen-sidebar-foot-buttons = [
-                            "downloads-button"
-                            "zen-workspaces-button"
-                            "zen-create-new-button"
-                        ];
-                        nav-bar = [
-                            "back-button"
-                            "forward-button"
-                            "stop-reload-button"
-                            "vertical-spacer"
-                            "urlbar-container"
-                            "unified-extensions-button"
-                            "_446900e4-71c2-419f-a6a7-df9c091e268b_-browser-action"
-                            "ublock0_raymondhill_net-browser-action"
-                            "addon_darkreader_org-browser-action"
-                        ];
-                        toolbar-menubar = [ "menubar-items" ];
-                        TabsToolbar = [ "tabbrowser-tabs" ];
-                        vertical-tabs = [ ];
-                        PersonalToolbar = [ "personal-bookmarks" ];
-                        unified-extensions-area = [ ];
-                    };
-                    seen = [
-                        "addon_darkreader_org-browser-action"
-                        "ublock0_raymondhill_net-browser-action"
-                        "_446900e4-71c2-419f-a6a7-df9c091e268b_-browser-action"
-                        "developer-button"
-                        "screenshot-button"
-                    ];
-                    dirtyAreaCache = [
-                        "unified-extensions-area"
-                        "nav-bar"
-                    ];
-                    currentVersion = 23;
-                    newElementCount = 5;
-                };
-                intl.accept_languages = "de,en-us,en";
-                browser.translations.neverTranslateLanguages = "en,de";
+                "extensions.autoDisableScopes" = 0;
+
+                "zen.welcome-screen.seen" = true;
+                "zen.tabs.show-newtab-vertical" = false;
+                "zen.view.use-single-toolbar" = false;
+                "zen.view.sidebar-expanded" = false;
+
+                "intl.accept_languages" = "de,en-us,en";
+                "browser.translations.neverTranslateLanguages" = "en,de";
+
+                # Does not work reliably, so do not use it for now
+                # "browser.uiCustomization.state" = "";
             };
 
             search = {
